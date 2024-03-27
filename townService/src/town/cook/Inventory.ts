a8adf6dfbd50b1e2f9914bb8cba3012412205b2b
Yinuo Wu <scott.wyn1215@gmail.com>
2024-03-27 15:49:25 -0400

    Inventory supports adding food

--- townService/src/town/cook/Inventory.ts
+++ townService/src/town/cook/Inventory.ts
@@ -1,46 +1,47 @@
+import { Food } from './interface/IFood';
 import { Ingredient } from './interface/IIngredient';
 
 // A users inventory of the ingredients they have available to cook with with a maximum of 6 ingredients
 
 export default class Inventory {
-  private _ingredients: [Ingredient, Ingredient, Ingredient, Ingredient, Ingredient, Ingredient];
+  private _grids: [
+    Ingredient | Food,
+    Ingredient | Food,
+    Ingredient | Food,
+    Ingredient | Food,
+    Ingredient | Food,
+    Ingredient | Food,
+  ];
 
-  constructor(ingredients: Ingredient[]) {
-    this._ingredients = [
-      ingredients[0],
-      ingredients[1],
-      ingredients[2],
-      ingredients[3],
-      ingredients[4],
-      ingredients[5],
-    ];
+  constructor(grids: Ingredient[] | Food[]) {
+    this._grids = [grids[0], grids[1], grids[2], grids[3], grids[4], grids[5]];
   }
 
   /**
    * Get the ingredients in the inventory
    */
-  get ingredients(): Ingredient[] {
-    return this._ingredients;
+  get grids(): (Ingredient | Food)[] {
+    return this._grids;
   }
 
   /**
    * Add an ingredient to the inventory
-   * @param ingredient The ingredient to add
+   * @param grid The ingredient to add
    */
-  addIngredient(ingredient: Ingredient): void {
-    if (this._ingredients.length < 6) {
-      this._ingredients.push(ingredient);
+  addIngredient(grid: Ingredient | Food): void {
+    if (this._grids.length < 6) {
+      this._grids.push(grid);
     }
   }
 
   /**
    * Remove an ingredient from the inventory
-   * @param ingredient The ingredient to remove
+   * @param grid The ingredient to remove
    */
-  removeIngredient(ingredient: Ingredient): void {
-    const index = this._ingredients.findIndex(i => i._id.$oid === ingredient._id.$oid);
+  removeIngredient(grid: Ingredient | Food): void {
+    const index = this._grids.findIndex(i => i._id.$oid === grid._id.$oid);
     if (index !== -1) {
-      this._ingredients.splice(index, 1);
+      this._grids.splice(index, 1);
     }
   }
 }
